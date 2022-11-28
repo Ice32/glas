306e07d6e124f0d3539744d083ad67e46de2d5de
Kenan Selimovic <kenanselimovic@gmail.com>
2022-11-28 09:12:58 +0100

    Fix http client not connecting on new emulator

--- lib/api/glas_http_client.dart
+++ lib/api/glas_http_client.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 import 'package:http/http.dart';
 
 class GlasHttpClient {
-  static const baseUrl = "http://localhost:8080";
+  static const baseUrl = "http://10.0.2.2:8080";
 
   Future<Response> post(String path, Map<String, String> body) {
     return Client().post(Uri.parse("$baseUrl/$path"),
@@ -13,6 +13,6 @@ class GlasHttpClient {
 
   Future<Response> get(String path) {
     return Client().get(Uri.parse("$baseUrl/$path"),
-        headers: {'Content-Type': 'application/json; charset=UTF-8'});
+        headers: {'Accept': 'application/json; charset=UTF-8'});
   }
 }
